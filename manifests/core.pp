aa2b3af2f6b86795a7c7f4bca8b33a0b94d9831d
grumpycat <contact@ladenis.fr>
2016-08-07 01:16:21 +0200

    Manage external drive

--- manifests/core.pp
+++ manifests/core.pp
@@ -16,4 +16,22 @@ class mypi::core {
     mode => '0770',
   }
 
+  file { "$::mypi::params::extdrivepath":
+    ensure => directory,
+    mode => '0755',
+  }
+
+  user { 'pi':
+    ensure => absent,
+  }
+
+  if 'sda' in "$blockdevices" {
+    file_line { 'fstab':
+      ensure => present,
+      path   => '/etc/fstab',
+      line   => "$::mypi::params::extdrive $::mypi::params::extdrivepath ext4 defaults,noatime,nodiratime,nodev,nofail  0 1",
+    }
+  }
+
+
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,5 +3,7 @@
 class mypi::params {
 
   $packages = [ 'vim', 'git', ]
+  $extdrive = '/dev/sda'
+  $extdrivepath = '/mnt/usbdrive01'
 
 }
