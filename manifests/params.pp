2304c6146d1fa4830d2a9bdca5c4bc6e22801b35
grumpycat <contact@ladenis.fr>
2016-08-26 20:48:41 +0200

    apt-file and dnsutils packages added

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,7 @@
 #
 class mypi::params {
 
+  $packages = [ 'vim', 'git', 'nodejs', 'npm', 'apt-transport-https', 'apt-file', 'dnsutils', ]
   $extdrive = '/dev/sda'
   $extdrivepath = '/mnt/usbdrive01'
 
