8e00f8c63a89b0116661e9f5dd252496eb5b8eac
grumpycat <contact@ladenis.fr>
2016-09-04 22:59:34 +0200

    Letsencrypt tls path

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,8 +6,8 @@ class mypi::params {
   $extdrive         = '/dev/sda'
   $extdrivepath     = '/mnt/usbdrive01'
   $hostname         = 'eagleusb'
-  $default_tls_crt  = '/etc/ssl/certs/let-front.pem'
-  $default_tls_key  = '/etc/ssl/private/let-front.key'
+  $default_tls_crt  = '/etc/letsencrypt/live/morningopenstack.org/fullchain.pem'
+  $default_tls_key  = '/etc/letsencrypt/live/morningopenstack.org/privkey.pem'
   $certbot_vtag     = 'v0.8.1'
 
 }
